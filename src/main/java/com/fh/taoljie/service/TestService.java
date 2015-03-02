e6e4488fefc5163162a4d4dbad2f644d4579858b
wanghongfei <brucewhf@gmail.com>
2015-03-02 18:06:51 +0800

    first commit

---
+++ src/main/java/com/fh/taoljie/service/TestService.java
@@ -0,0 +1,7 @@
+package com.fh.taoljie.service;
+
+/**
+ * Created by whf on 15-3-2.
+ */
+public interface TestService {
+}
---
+++ src/main/java/com/fh/taoljie/service/impl/DefaultTestService.java
@@ -0,0 +1,11 @@
+package com.fh.taoljie.service.impl;
+
+import com.fh.taoljie.service.TestService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by whf on 15-3-2.
+ */
+@Service
+public class DefaultTestService implements TestService {
+}
