941be732cd146131c0029004790f6039ffc5cf8f
wanghongfei <brucewhf@gmail.com>
2015-03-06 20:47:14 +0800

    实现NewsService接口方法,未测试

---
+++ src/main/java/com/fh/taolijie/service/impl/DefaultNewsService.java
@@ -0,0 +1,105 @@
+package com.fh.taolijie.service.impl;
+
+import com.fh.taolijie.controller.dto.NewsDto;
+import com.fh.taolijie.domain.NewsEntity;
+import com.fh.taolijie.service.NewsService;
+import com.fh.taolijie.utils.Constants;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by wanghongfei on 15-3-6.
+ */
+@Repository
+public class DefaultNewsService implements NewsService {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<NewsDto> getNewsList(int firstResult, int capacity) {
+        int cap = capacity;
+        if (0 == capacity) {
+            cap = Constants.PAGE_CAPACITY;
+        }
+
+        em.createNamedQuery("newsEntity.findAll")
+                .setFirstResult(firstResult)
+                .setMaxResults(cap);
+        return null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<NewsDto> getNewsList(Date uptime, int firstResult, int capacity) {
+        int cap = capacity;
+        if (0 == cap) {
+            cap = Constants.PAGE_CAPACITY;
+        }
+
+        List<NewsEntity> newsList = em.createNamedQuery("newsEntity.findByDate", NewsEntity.class)
+                .setParameter("date", uptime)
+                .setFirstResult(firstResult)
+                .setMaxResults(cap)
+                .getResultList();
+
+        List<NewsDto> dtoList = new ArrayList<>();
+        for (NewsEntity news : newsList) {
+            dtoList.add(makeNewsDto(news));
+        }
+
+        return dtoList;
+    }
+
+    private NewsDto makeNewsDto(NewsEntity news) {
+        NewsDto dto = new NewsDto(news.getTitle(), news.getContent(), news.getPicturePath(),
+                news.getTime(), news.getHeadPicturePath(), null);
+        dto.setMemberId(news.getMember().getId());
+
+        return dto;
+    }
+
+    /**
+     * 不更新time和memberId
+     * @param news
+     * @param dto
+     */
+    private void updateNewsDto(NewsEntity news, NewsDto dto) {
+        dto.setTitle(news.getTitle());
+        dto.setContent(news.getContent());
+        dto.setPicturePath(news.getPicturePath());
+        dto.setHeadPicturePath(news.getHeadPicturePath());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public NewsDto findNews(Integer newsId) {
+        NewsEntity news = em.find(NewsEntity.class, newsId);
+        return makeNewsDto(news);
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+    public boolean updateNews(Integer newsId, NewsDto newsDto) {
+        NewsEntity news = em.find(NewsEntity.class, newsId);
+        updateNewsDto(news, newsDto);
+
+        return true;
+    }
+
+    @Override
+    public boolean deleteNews(Integer newsId) {
+        NewsEntity news = em.getReference(NewsEntity.class, newsId);
+        em.remove(news);
+
+        return true;
+    }
+
+}
