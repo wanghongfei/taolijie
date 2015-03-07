2e831ccccc272b546984e1a3850a5c486814f43a
wynfrith <wangfucheng56@gmail.com>
2015-03-07 20:09:18 +0800

    添加用来测试的Controller

---
+++ src/main/java/com/fh/taolijie/controller/TestController.java
@@ -0,0 +1,75 @@
+package com.fh.taolijie.controller;
+
+import cn.fh.security.credential.Credential;
+import cn.fh.security.credential.DefaultCredential;
+import cn.fh.security.utils.CredentialUtils;
+import com.fh.taolijie.controller.dto.GeneralMemberDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 用于测试一些不会用的功能
+ * Created by wynfrith on 15-3-7.
+ */
+@Controller
+public class TestController {
+
+    /**
+     * 用户登陆页面
+     * @return
+     */
+    @RequestMapping("/test/login")
+    public String login(){
+        return "test/login";
+    }
+
+    /**
+     * 登陆请求
+     * @param mem
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "/test/login", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public @ResponseBody String login(GeneralMemberDto mem,
+                        HttpSession session){
+        JsonObject json = null;
+        json = Json.createObjectBuilder()
+                .add("result",true)
+                .add("message","登陆成功")
+                .build();
+
+        Credential credential = new DefaultCredential(1,mem.getUsername(),"nickname",100);
+        CredentialUtils.createCredential(session,credential);
+
+        return json.toString();
+    }
+
+
+    /**
+     * 管理员
+     * @param session
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/test/admin")
+   public String admin(HttpSession session,Model model) {
+        System.out.println(session);
+        System.out.println(model);
+        if(session!=null){
+            Credential credential = CredentialUtils.getCredential(session);
+            System.out.println("用户名 = = "+credential.getUsername());
+            model.addAttribute("username",credential.getUsername());
+        }
+        else{
+            System.out.println("session为空?");
+        }
+       return "test/admin";
+   }
+}
