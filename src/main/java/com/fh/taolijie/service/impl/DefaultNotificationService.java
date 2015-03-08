5ca9bdb0c70e6784f18bf03a0452128ebb9abcc7
wanghongfei <brucewhf@gmail.com>
2015-03-09 00:59:31 +0800

    实现通知CRUD

---
+++ src/main/java/com/fh/taolijie/service/impl/DefaultNotificationService.java
@@ -0,0 +1,109 @@
+package com.fh.taolijie.service.impl;
+
+import com.fh.taolijie.controller.dto.NotificationDto;
+import com.fh.taolijie.domain.NotificationEntity;
+import com.fh.taolijie.service.NotificationService;
+import com.fh.taolijie.service.repository.MemberRepo;
+import com.fh.taolijie.service.repository.NotificationRepo;
+import com.fh.taolijie.utils.CollectionUtils;
+import com.fh.taolijie.utils.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by wanghongfei on 15-3-9.
+ */
+@Service
+public class DefaultNotificationService implements NotificationService {
+    @Autowired
+    NotificationRepo notRepo;
+    @Autowired
+    MemberRepo memRepo;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<NotificationDto> getNotificationList(Integer memId, int firstResult, int capacity) {
+        int cap = capacity;
+        if (cap <= 0) {
+            cap = Constants.PAGE_CAPACITY;
+        }
+
+        Page<NotificationEntity> noList = notRepo.findByMember(memRepo.getOne(memId), new PageRequest(firstResult, cap));
+
+        return CollectionUtils.transformCollection(noList, NotificationDto.class, (entity) -> {
+            return makeDto(entity);
+        });
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<NotificationDto> getNotificationList(Integer memId, boolean isRead, int firstResult, int capacity) {
+        int cap = capacity;
+        if (cap <= 0) {
+            cap = Constants.PAGE_CAPACITY;
+        }
+
+        Page<NotificationEntity> noList = notRepo.findByMemberAndIsRead(memRepo.getOne(memId), isRead ? 1 : 0, new PageRequest(firstResult, cap));
+
+        return CollectionUtils.transformCollection(noList, NotificationDto.class, (entity) -> {
+            return makeDto(entity);
+        });
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<NotificationDto> getNotificationList(Integer memId, Date time, int firstResult, int capacity) {
+        int cap = capacity;
+        if (cap <= 0) {
+            cap = Constants.PAGE_CAPACITY;
+        }
+
+        Page<NotificationEntity> noList = notRepo.findAfterTheTime(memRepo.getOne(memId), time, new PageRequest(firstResult, cap));
+
+        return CollectionUtils.transformCollection(noList, NotificationDto.class, (entity) -> {
+            return makeDto(entity);
+        });
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public NotificationDto findNotification(Integer notificationId) {
+        return makeDto(notRepo.findOne(notificationId));
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+    public boolean deleteNotification(Integer notificationId) {
+        notRepo.delete(notificationId);
+
+        return true;
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+    public boolean markAsRead(Integer noticicationId) {
+        NotificationEntity no = notRepo.getOne(noticicationId);
+        no.setIsRead(1);
+
+        return true;
+    }
+
+    private NotificationDto makeDto(NotificationEntity no) {
+        NotificationDto dto = new NotificationDto();
+        dto.setTitle(no.getTitle());
+        dto.setContent(no.getContent());
+        dto.setIsRead(no.getIsRead());
+        dto.setTime(no.getTime());
+
+        dto.setMemberId(no.getMember().getId());
+
+        return dto;
+    }
+}
--- src/main/java/com/fh/taolijie/service/repository/NotificationRepo.java
+++ src/main/java/com/fh/taolijie/service/repository/NotificationRepo.java
@@ -1,10 +1,21 @@
 package com.fh.taolijie.service.repository;
 
+import com.fh.taolijie.domain.MemberEntity;
 import com.fh.taolijie.domain.NotificationEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
 
 /**
  * Created by wanghongfei on 15-3-8.
  */
 public interface NotificationRepo extends JpaRepository<NotificationEntity, Integer> {
+    Page<NotificationEntity> findByMember(MemberEntity member, Pageable pageable);
+    Page<NotificationEntity> findByMemberAndIsRead(MemberEntity member, Integer isRead, Pageable pageable);
+
+    @Query("SELECT no FROM NotificationEntity no WHERE no.time > ?2")
+    Page<NotificationEntity> findAfterTheTime(MemberEntity member, Date thatTime, Pageable pageable);
 }
