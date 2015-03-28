2f41acf1e7f7fb8a238bfc3185ff4cf7a5b93aed
wynfrith <wangfucheng56@gmail.com>
2015-03-28 21:02:20 +0800

    在credential中记录用户的id

--- src/main/java/com/fh/taolijie/utils/TaolijieCredential.java
+++ src/main/java/com/fh/taolijie/utils/TaolijieCredential.java
@@ -6,11 +6,16 @@ import cn.fh.security.credential.DefaultCredential;
  * Created by wynfrith on 15-3-7.
  */
 public class TaolijieCredential extends DefaultCredential{
-    public TaolijieCredential(String username, String nickName, Integer credits) {
-        super(null, username, nickName, credits);
-    }
+//    public TaolijieCredential() {
+//        super(null, username, nickName, credits);
+//    }
     public TaolijieCredential(String username){
         super(null,username,null,null);
+    }
+    public TaolijieCredential(int id,String username){
+        /*DefaultCredential类的构造器缺少id的赋值,暂时这样做*/
+        super(null,username,null,null);
+        super.id = id;
     }
 
 }
