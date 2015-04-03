2fb76330a214ab634dd4a4aa1508846d43a4cfdd
wanghongfei <brucewhf@gmail.com>
2015-04-03 16:14:40 +0800

    添加cachePage工具类

---
+++ src/main/java/com/fh/taolijie/utils/CachePageUtils.java
@@ -0,0 +1,33 @@
+package com.fh.taolijie.utils;
+
+import java.io.IOException;
+
+/**
+ * Created by wanghongfei on 15-4-3.
+ */
+public class CachePageUtils {
+    private CachePageUtils() {}
+
+    /**
+     * 保存页面cache的目录
+     */
+    //public static String CACHED_PAGE_DIR = "/var";
+    public static String CACHED_PAGE_DIR = "/home/whf/test";
+
+    public static void flush(String url) throws IOException {
+        StringBuilder cmd = new StringBuilder("wget");
+        cmd.append(" ");
+        cmd.append(url);
+        cmd.append(" ");
+        cmd.append("-O");
+        cmd.append(" ");
+        cmd.append(CACHED_PAGE_DIR);
+
+        Process process = Runtime.getRuntime().exec(cmd.toString());
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/fh/taolijie/test/other/CachePageTest.java
@@ -0,0 +1,14 @@
+package com.fh.taolijie.test.other;
+
+import com.fh.taolijie.utils.CachePageUtils;
+import org.junit.Test;
+
+/**
+ * Created by wanghongfei on 15-4-3.
+ */
+public class CachePageTest {
+    @Test
+    public void testCacehPage() throws Exception {
+        CachePageUtils.flush("http://localhost:8080/");
+    }
+}
