a7598cc691b929104596fbe50fff093e367257a6
wanghongfei <brucewhf@gmail.com>
2015-06-03 15:59:33 +0800

    二手信息CRUD

--- src/main/java/com/fh/taolijie/dao/mapper/JobPostCategoryMapper.java
+++ src/main/java/com/fh/taolijie/dao/mapper/JobPostCategoryMapper.java
@@ -1,12 +1,14 @@
 package com.fh.taolijie.dao.mapper;
 
 import com.fh.taolijie.domain.JobPostCategoryModel;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by wanghongfei on 15-5-31.
  */
+@Repository
 public interface JobPostCategoryMapper {
     JobPostCategoryModel getCategoryById(Integer categoryId);
 
--- src/main/java/com/fh/taolijie/dao/mapper/ReviewMapper.java
+++ src/main/java/com/fh/taolijie/dao/mapper/ReviewMapper.java
@@ -1,12 +1,14 @@
 package com.fh.taolijie.dao.mapper;
 
 import com.fh.taolijie.domain.ReviewModel;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by wanghongfei on 15-5-31.
  */
+@Repository
 public interface ReviewMapper {
     List<ReviewModel> getReviewByIdInBatch(List<Integer> idList);
     List<ReviewModel> getReviewByJobPost(ReviewModel model);
