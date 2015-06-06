fe46e43400cbafbcea34709af6ee4b70d11db239
wanghongfei <brucewhf@gmail.com>
2015-06-06 14:36:24 +0800

    implement job post service interface

--- src/main/java/com/fh/taolijie/service/JobPostService.java
+++ src/main/java/com/fh/taolijie/service/JobPostService.java
@@ -2,6 +2,7 @@ package com.fh.taolijie.service;
 
 import com.fh.taolijie.controller.dto.JobPostDto;
 import com.fh.taolijie.domain.JobPostModel;
+import com.fh.taolijie.domain.JobPostModelWithBLOBs;
 import com.fh.taolijie.utils.Constants;
 import com.fh.taolijie.utils.ObjWrapper;
 
@@ -16,21 +17,21 @@ public interface JobPostService extends PageService {
      * 查找所有兼职
      * @return
      */
-    List<JobPostDto> getAllJobPostList(int firstResult, int capacity, ObjWrapper wrapper);
+    List<JobPostModel> getAllJobPostList(int firstResult, int capacity, ObjWrapper wrapper);
 
     /**
      * 获取某个用户发的所有兼职帖子
      * @param memId 用户id
      * @return
      */
-    List<JobPostDto> getJobPostListByMember(Integer memId , int firstResult, int capacity, ObjWrapper wrapper);
+    List<JobPostModel> getJobPostListByMember(Integer memId , int firstResult, int capacity, ObjWrapper wrapper);
 
     /**
      * 获取某分类下所有的兼职帖子
      * @param cateId
      * @return
      */
-    List<JobPostDto> getJobPostListByCategory(Integer cateId, int firstResult, int capacity, ObjWrapper wrapper);
+    List<JobPostModel> getJobPostListByCategory(Integer cateId, int firstResult, int capacity, ObjWrapper wrapper);
 
     /**
      * 查询未审核的post
@@ -39,20 +40,20 @@ public interface JobPostService extends PageService {
      * @param wrapper
      * @return
      */
-    List<JobPostDto> getUnverifiedPostList(int firstResult, int capacity, ObjWrapper wrapper);
+    List<JobPostModel> getUnverifiedPostList(int firstResult, int capacity, ObjWrapper wrapper);
 
     /**
      * 根据多个id批量查询兼职信息
      * @param ids
      * @return
      */
-    List<JobPostDto> getPostListByIds(Integer... ids);
+    List<JobPostModel> getPostListByIds(Integer... ids);
 
     /**
      * 查询被投诉的兼职信息，值最高的在前
      * @return
      */
-    List<JobPostDto> getByComplaint(int firstResult, int capacity, ObjWrapper wrapper);
+    List<JobPostModel> getByComplaint(int firstResult, int capacity, ObjWrapper wrapper);
 
     /**
      * 根据条件查找兼职信息.
@@ -67,7 +68,7 @@ public interface JobPostService extends PageService {
      * @param capacity
      * @return
      */
-    List<JobPostDto> getAndFilter(Integer categoryId, Constants.WayToPay wayToPay, boolean orderByDate, boolean orderByPageVisit, Integer schoolId, int firstResult, int capacity, ObjWrapper wrapper);
+    List<JobPostModel> getAndFilter(Integer categoryId, Constants.WayToPay wayToPay, boolean orderByDate, boolean orderByPageVisit, Integer schoolId, int firstResult, int capacity, ObjWrapper wrapper);
 
     /**
      * 根据兼职信息的某个字段执行模糊查询
@@ -75,14 +76,14 @@ public interface JobPostService extends PageService {
      * @param includeString
      * @return
      */
-    List<JobPostDto> runSearch(String field, String includeString, int firstResult, int capacity, ObjWrapper wrapper);
+    List<JobPostModel> runSearch(JobPostModel model, int firstResult, int capacity, ObjWrapper wrapper);
 
     /**
      * 根据id查找某个兼职帖子
      * @param postId
      * @return
      */
-    JobPostDto findJobPost(Integer postId);
+    JobPostModel findJobPost(Integer postId);
 
     /**
      * 投诉数+1
@@ -114,7 +115,7 @@ public interface JobPostService extends PageService {
      * 发布帖子
      * @param dto
      */
-    void addJobPost(JobPostDto dto);
+    void addJobPost(JobPostModelWithBLOBs model);
 
     /**
      * 修改兼职帖子, 无法修改评论信息
@@ -122,7 +123,7 @@ public interface JobPostService extends PageService {
      * @param postDto
      * @return
      */
-    boolean updateJobPost(Integer postId, JobPostDto postDto);
+    boolean updateJobPost(Integer postId, JobPostModelWithBLOBs model);
 
     /**
      * 删除兼职帖子, 同时删除帖子下的评论
