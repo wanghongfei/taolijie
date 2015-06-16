44dee07371471ec3f189263d92cd601fca6344ea
wanghongfei <brucewhf@gmail.com>
2015-06-16 14:16:11 +0800

    默认先取消缓存

--- src/test/java/com/fh/taolijie/test/MySqlConfigBean.java
+++ src/test/java/com/fh/taolijie/test/MySqlConfigBean.java
@@ -1,27 +1,17 @@
 package com.fh.taolijie.test;
 
-import com.fh.taolijie.domain.MemberModel;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by wanghongfei on 15-4-10.
@@ -32,17 +22,17 @@ import java.util.Map;
         "com.fh.taolijie.service",
         "com.fh.taolijie.dao.mapper",
 })
-@EnableCaching
+//@EnableCaching
 @MapperScan( annotationClass = Repository.class, basePackages = "com.fh.taolijie.dao.mapper", sqlSessionFactoryRef = "sqlSessionFactory")
 public class MySqlConfigBean {
     @Autowired
     private DataSource dataSource;
 
-    @Autowired
+/*    @Autowired
     private JedisConnectionFactory jedisConnectionFactory;
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate redisTemplate;*/
 
     @Bean
     public DataSource dataSource() {
@@ -72,6 +62,7 @@ public class MySqlConfigBean {
     }
 
 
+/*
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
@@ -106,6 +97,7 @@ public class MySqlConfigBean {
 
         return redisCacheManager;
     }
+*/
 
 /*    @Bean
     public MapperScannerConfigurer scanner() {
--- src/test/java/com/fh/taolijie/test/dao/mapper/JobPostServiceTest.java
+++ src/test/java/com/fh/taolijie/test/dao/mapper/JobPostServiceTest.java
@@ -1,5 +1,6 @@
 package com.fh.taolijie.test.dao.mapper;
 
+import com.fh.taolijie.dao.mapper.JobPostModelMapper;
 import com.fh.taolijie.domain.JobPostModel;
 import com.fh.taolijie.service.JobPostService;
 import com.fh.taolijie.service.impl.DefaultJobPostService;
@@ -21,21 +22,39 @@ public class JobPostServiceTest extends BaseSpringDataTestClass {
     @Autowired
     JobPostService postService;
 
+    @Autowired
+    JobPostModelMapper jobMapper;
+
     @Test
     public void testGet() {
         List<JobPostModel> list = postService.getAllJobPostList(0, 100, null);
         Assert.assertFalse(list.isEmpty());
 
         Assert.assertFalse(postService.getJobPostListByMember(1, 0, 100, null).isEmpty() );
+
+        long tot = jobMapper.countGetAll();
+        Assert.assertNotEquals(0, tot);
+    }
+
+    @Test
+    public void testFindBy() {
+        JobPostModel job = new JobPostModel();
+        job.setIntroduce("998");
+
+        List<JobPostModel> list = jobMapper.findBy(job);
+        long tot = jobMapper.countFindBy(job);
+        Assert.assertEquals(tot, list.size());
     }
 
     @Test
     public void testSearch() {
         JobPostModel job = new JobPostModel();
-        job.setTitle("快");
+        job.setTitle("");
         List<JobPostModel> list = postService.runSearch(job, 0, 100, null);
+        long tot = jobMapper.countSearchBy(job);
 
         Assert.assertFalse(list.isEmpty());
+        Assert.assertEquals(list.size(), tot);
     }
 
 }
