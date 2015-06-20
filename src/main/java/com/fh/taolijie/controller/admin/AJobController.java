22eb2fffdb9940808df9c335f5e4349b0f67a519
wynfrith <wangfucheng56@gmail.com>
2015-06-20 22:24:25 +0800

    修复兼职二手简历的删除

--- src/main/java/com/fh/taolijie/controller/admin/AJobController.java
+++ src/main/java/com/fh/taolijie/controller/admin/AJobController.java
@@ -9,10 +9,7 @@ import com.fh.taolijie.utils.json.JsonWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -49,9 +46,9 @@ public class AJobController {
     /**
      * 删除用户的兼职
      */
-    @RequestMapping(value = "/del",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
+    @RequestMapping(value = "/del/{id}",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
     public @ResponseBody
-    String deleteInfo(@RequestParam int id){
+    String deleteInfo(@PathVariable int id){
             JobPostModel job = jobPostService.findJobPost(id);
 
             if(!jobPostService.deleteJobPost(id)){
--- src/main/java/com/fh/taolijie/controller/admin/AShController.java
+++ src/main/java/com/fh/taolijie/controller/admin/AShController.java
@@ -10,10 +10,7 @@ import com.fh.taolijie.utils.json.JsonWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -50,9 +47,9 @@ public class AShController {
     /**
      * 删除用户的二手
      */
-    @RequestMapping(value = "/del",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
+    @RequestMapping(value = "/del/{id}",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
     public @ResponseBody
-    String deleteInfo(@RequestParam int id){
+    String deleteInfo(@PathVariable int id){
         SHPostModel sh = shPostService.findPost(id);
 
         if(!shPostService.deletePost(id)){
