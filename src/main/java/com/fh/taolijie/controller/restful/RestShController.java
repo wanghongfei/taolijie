e8855e0333693a2950a2295ba68e60ceecd0400d
wanghongfei <brucewhf@gmail.com>
2015-06-24 10:01:09 +0800

    implement part of JSON interfaces

--- src/main/java/com/fh/taolijie/controller/restful/RestShController.java
+++ src/main/java/com/fh/taolijie/controller/restful/RestShController.java
@@ -40,29 +40,17 @@ public class RestShController {
      */
     @RequestMapping(value = "/category/{categoryId}", produces = Constants.Produce.JSON)
     public ResponseText getByCategory(@PathVariable("categoryId") Integer categoryId,
+                                      @RequestParam(defaultValue = "false") Boolean pageView,
                                       @RequestParam(defaultValue = "0") Integer pageNumber,
                                       @RequestParam(defaultValue = Constants.PAGE_CAPACITY + "") Integer pageSize) {
 
-        List<SHPostModel> shList = shService.getPostList(categoryId, pageNumber, pageSize, null);
+        List<SHPostModel> shList = null;
+        if (false == pageView) {
+            shService.getPostList(categoryId, pageNumber, pageSize, null);
+        } else {
+            shList = shService.getAndFilter(categoryId, pageView, pageNumber, pageSize, null);
+        }
 
-        return new ResponseText(shList);
-    }
-
-    /**
-     * 根据分类查询。可指定是否根据pageView排序
-     * @param categoryId
-     * @param pageView true表示 ORDER BY pageView DESC
-     * @param pageNumber
-     * @param pageSize
-     * @return
-     */
-    @RequestMapping(value = "/category/{categoryId}", method = RequestMethod.POST, produces = Constants.Produce.JSON)
-    public ResponseText getByCategory(@PathVariable("categoryId") Integer categoryId,
-                                      @RequestParam(defaultValue = "true") Boolean pageView,
-                                      @RequestParam(defaultValue = "0") Integer pageNumber,
-                                      @RequestParam(defaultValue = Constants.PAGE_CAPACITY + "") Integer pageSize) {
-
-        List<SHPostModel> shList = shService.getAndFilter(categoryId, pageView, pageNumber, pageSize, null);
         return new ResponseText(shList);
     }
 
@@ -75,8 +63,8 @@ public class RestShController {
      * @param pageSize
      * @return
      */
-    @RequestMapping(value = "/user/{userId}", produces = Constants.Produce.JSON)
-    public ResponseText getByMember(@PathVariable("userId") Integer userId,
+    @RequestMapping(value = "/user/{memberId}", produces = Constants.Produce.JSON)
+    public ResponseText getByMember(@PathVariable("memberId") Integer userId,
                                     @RequestParam(defaultValue = "true") Boolean filter,
                                       @RequestParam(defaultValue = "0") Integer pageNumber,
                                       @RequestParam(defaultValue = Constants.PAGE_CAPACITY + "") Integer pageSize) {
