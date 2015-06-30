e7e8a5301d34ae536b48d553edfacc729aab2bc2
lankeren <x@lankeren.com>
2015-06-30 15:57:33 +0800

    sh list img bug fix

--- src/main/webapp/WEB-INF/views/pc/assets/scripts/sh-pic.js
+++ src/main/webapp/WEB-INF/views/pc/assets/scripts/sh-pic.js
@@ -4,7 +4,7 @@ var $item;
 var picIds;
 for( var i = 0; i<$shList.length; i++) {
     $item = $($shList[i]);
-    picIds = $item.data('pid').split(';');
+    picIds = ($item.data('pid') + '').split(';');
     if(picIds[0] > 0) {
         url = '/static/images/users/' + picIds[0];
     } else {
