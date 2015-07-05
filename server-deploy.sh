6e4918a9c73bd658b265446bc2ed4f375ce39ac4
whf <brucewhf@gmail.com>
2015-07-05 14:44:38 +0800

    添加一个自动部署脚本

---
+++ server-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "deploying static resources..."
+rm -rf /www/resources/*
+cp -r /root/projects/taolijie/src/main/webapp/WEB-INF/views/pc/assets/about /www/resources
+cp -r /root/projects/taolijie/src/main/webapp/WEB-INF/views/pc/assets/admin /www/resources
+cp -r /root/projects/taolijie/src/main/webapp/WEB-INF/views/pc/assets/fonts /www/resources
+cp -r /root/projects/taolijie/src/main/webapp/WEB-INF/views/pc/assets/images /www/resources
+cp -r /root/projects/taolijie/src/main/webapp/WEB-INF/views/pc/assets/scripts /www/resources
+cp -r /root/projects/taolijie/src/main/webapp/WEB-INF/views/pc/assets/styles /www/resources
+echo "done"
+
+echo "deploying war file..."
+rm -rf /opt/apache-tomcat-7.0.62/webapps/ROOT/*
+cp /root/projects/taolijie/target/taolijie.war /tmp
+cd /tmp
+unzip taolijie.war
+mv taolijie/ /opt/apache-tomcat-7.0.62/webapps/ROOT/
+echo "done"
