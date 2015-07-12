95e22ac7bdebb78e04650c7274bf93bf49c97934
whf <brucewhf@gmail.com>
2015-07-12 16:27:54 +0800

    make qq optional

--- src/main/java/com/fh/taolijie/domain/SHPostModel.java
+++ src/main/java/com/fh/taolijie/domain/SHPostModel.java
@@ -162,8 +162,8 @@ public class SHPostModel extends Pageable {
     @NotNull
     @Length(max = 10)
     private String contactName;
-    @NotNull
-    @Length(max = 20)
+    //@NotNull
+    //@Length(max = 20)
     private String contactQq;
     @NotNull
     @Length(max = 10)
