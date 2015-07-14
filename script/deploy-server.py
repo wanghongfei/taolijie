7aa0504d15e2dda28506e9f16f399a275c9c8230
whf <brucewhf@gmail.com>
2015-07-15 00:43:26 +0800

    只有master的push事件才会重新部署

--- script/deploy-server.py
+++ script/deploy-server.py
@@ -9,11 +9,13 @@ import time
 import BaseHTTPServer
 import subprocess
 import logging
+import json
 
 # 配置logger
 FORMAT = "%(asctime)-15s %(message)s"
 CUR_TIME = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-logging.basicConfig(filename="/data/taolijie/scripts-log/deploy-server_%s.log" % CUR_TIME, format=FORMAT, level=logging.DEBUG)
+#logging.basicConfig(filename="/data/taolijie/scripts-log/deploy-server_%s.log" % CUR_TIME, format=FORMAT, level=logging.DEBUG)
+logging.basicConfig(filename="deploy-server_%s.log" % CUR_TIME, format=FORMAT, level=logging.DEBUG)
 
 
 # 该函数用于执行外部命令来部署工程
@@ -33,13 +35,30 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
         logging.info('client port:%s', self.client_address[1])
         logging.info('method:%s', self.command)
 
+        # 读取请求内容
+        json_str = self.rfile.read(int(self.headers['Content-Length']))
+        json_dict = None
+        try:
+            json_dict = json.loads(json_str)
+        except:
+            logging.error('decoding JSON failed; [%s]', json_str)
+            self.send_error(400)
+            return
+
+        branch_name = json_dict['ref']
+        # 判断是否是master分支
+        if not branch_name == 'refs/heads/master':
+            logging.info('[%s] is not master branch, do nothing', branch_name)
+            self.send_response(200)
+            return
+
         # 启动新线程执行命令
         thread.start_new_thread(deploy, ())
 
         self.send_response(200)
 
     def log_error(self, format, *args):
-        logging.error(format, args)
+        logging.error(args)
 
 
 # 启动HTTP Server
