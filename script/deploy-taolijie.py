a3bded96d5ea55466c54d5de5f5a63504383b5c8
whf <brucewhf@gmail.com>
2015-07-14 19:12:34 +0800

    modify deploy script

--- script/deploy-taolijie.py
+++ script/deploy-taolijie.py
@@ -7,7 +7,7 @@ import shutil
 
 import subprocess
 
-if sys.argv[1]:
+if len(sys.argv) == 2:
     if sys.argv[1] == 'pull':
         print '[INFO] pulling code from github'
         outcome = subprocess.check_output(('git', 'pull', 'origin', 'master'), stderr=subprocess.STDOUT)
@@ -30,40 +30,40 @@ def exe_cmd(args):
 
 print '[INFO] rebuild project'
 os.chdir('/root/projects/taolijie')
-outcome = exe_cmd(('mvn', 'clean, install', '-Dmaven.test.skip=true'))
+outcome = exe_cmd(('mvn', 'clean', 'install', '-Dmaven.test.skip=true'))
 print outcome
 
 
 
 print '[INFO] deploy static resources'
-STATIC_RESOURCE_PATH = '/www/resources'
+STATIC_RESOURCE_PATH = '/www/resources/'
 for dir_name in os.listdir(STATIC_RESOURCE_PATH):
     shutil.rmtree(dir_name)
 ASSET_PATH = '/root/projects/taolijie/src/main/webapp/WEB-INF/views/pc/assets'
-shutil.copytree(ASSET_PATH.join('/about'), STATIC_RESOURCE_PATH)
-shutil.copytree(ASSET_PATH.join('/admin'), STATIC_RESOURCE_PATH)
-shutil.copytree(ASSET_PATH.join('/fonts'), STATIC_RESOURCE_PATH)
-shutil.copytree(ASSET_PATH.join('/images'), STATIC_RESOURCE_PATH)
-shutil.copytree(ASSET_PATH.join('/scripts'), STATIC_RESOURCE_PATH)
-shutil.copytree(ASSET_PATH.join('/styles'), STATIC_RESOURCE_PATH)
+shutil.copytree(ASSET_PATH.join('/about'), STATIC_RESOURCE_PATH + 'about')
+shutil.copytree(ASSET_PATH.join('/admin'), STATIC_RESOURCE_PATH + 'admin')
+shutil.copytree(ASSET_PATH.join('/fonts'), STATIC_RESOURCE_PATH + 'fonts')
+shutil.copytree(ASSET_PATH.join('/images'), STATIC_RESOURCE_PATH + 'images')
+shutil.copytree(ASSET_PATH.join('/scripts'), STATIC_RESOURCE_PATH + 'scripts')
+shutil.copytree(ASSET_PATH.join('/styles'), STATIC_RESOURCE_PATH + 'styles')
 
 
 print '[INFO] shut tomcat down'
 TOMCAT_HOME = os.getenv('TOMCAT_HOME')
-TOMCAT_PATH = os.getenv('TOMCAT_HOME').join('/bin')
-outcome = exe_cmd((TOMCAT_PATH.join('shutdown.sh')))
+TOMCAT_PATH = os.getenv('TOMCAT_HOME') + '/bin'
+outcome = exe_cmd((TOMCAT_PATH + 'shutdown.sh'))
 print outcome
 
 print '[INFO] deploy WAR file'
-shutil.rmtree(TOMCAT_HOME.join('/webapps/ROOT'))
+shutil.rmtree(TOMCAT_HOME + '/webapps/ROOT')
 shutil.copy('/root/projects/taolijie/target/taolijie.war', '/tmp')
 os.chdir('/tmp')
-subprocess.call(('unzip', 'taolijie.war'))
-shutil.copytree('WEB-INF', TOMCAT_HOME.join('/webapps/ROOT'))
-shutil.copytree('META-INF', TOMCAT_HOME.join('/webapps/ROOT'))
+subprocess.call(('unzip', '-q', 'taolijie.war'))
+shutil.copytree('WEB-INF', TOMCAT_HOME + '/webapps/ROOT')
+shutil.copytree('META-INF', TOMCAT_HOME + '/webapps/ROOT')
 
 print '[INFO] start tomcat'
-outcome = exe_cmd(('startup.sh'))
+outcome = exe_cmd((TOMCAT_PATH + 'startup.sh'))
 print outcome
 
 print '[INFO] clean up'
