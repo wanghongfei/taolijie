32499753bef08ae2172252f2ba63052f328d01a7
whf <brucewhf@gmail.com>
2015-07-14 12:05:44 +0800

    update scripts

--- script/deploy-server.py
+++ script/deploy-server.py
@@ -1,3 +1,4 @@
+# encoding=UTF-8
 __author__ = 'whf'
 
 import sys
@@ -7,33 +8,34 @@ import BaseHTTPServer
 import subprocess
 import logging
 
-# configure logging
+# 配置logger
 FORMAT = "%(asctime)-15s  %(user)-8s %(message)s"
 CUR_TIME = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
 logging.basicConfig(filename="/data/taolijie/scripts-log/deploy-server-%s.log" % CUR_TIME, format=FORMAT)
 
 
+# 该函数用于执行外部命令来部署工程
 def deploy():
-    outcome = subprocess.check_output(('.', '/root/projects/taolijie/server-deploy.sh'))
+    outcome = subprocess.check_output(('/root/projects/taolijie/server-deploy.sh'))
     logging.info(outcome)
     return outcome
 
 
-# The request handler
+# HTTP请求处理类
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
-    # handle POST request
+    # 处理POST请求
     def do_POST(self):
         logging.info('client addr:%s', self.client_address[0])
         logging.info('client port:%s', self.client_address[1])
         logging.info('method:%s', self.command)
 
-        # execute command in a separate thread
+        # 启动新线程执行命令
         thread.start_new_thread(deploy)
 
         self.send_response(200)
 
 
-# start HTTP server
+# 启动HTTP Server
 server_addr = ('127.0.0.1', 9001)
 httpd = BaseHTTPServer.HTTPServer(server_addr, Handler)
 httpd.serve_forever()
--- server-deploy.sh
+++ server-deploy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+
+check_success() {
+    if [ "$?" != "0" ]; then
+        echo "stop build"
+        exit 1
+    fi
+}
+
 echo "building project..."
 git pull origin master
+check_success
+
 mvn clean install -Dmaven.test.skip=true
+check_success
 
 echo "deploying static resources..."
 rm -rf /www/resources/*
@@ -16,6 +27,7 @@ echo "done"
 
 # shut server down
 /opt/apache-tomcat-7.0.62/bin/shutdown.sh
+check_success
 
 echo "deploying war file..."
 rm -rf /opt/apache-tomcat-7.0.62/webapps/ROOT/*
@@ -30,4 +42,5 @@ rm taolijie.war
 
 # start server
 /opt/apache-tomcat-7.0.62/bin/startup.sh
+check_success
 echo "done"
