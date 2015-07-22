02015b960f36b651bef8388e782f30eb6e4cc8d4
whf <brucewhf@gmail.com>
2015-07-22 15:23:56 +0800

    完善关键字提取

--- searching/wordcut/cut.py
+++ searching/wordcut/cut.py
@@ -17,7 +17,7 @@ class KeywordGenerator:
     '''
     def gen_keywords(self, text):
         result_list = list(jieba.cut_for_search(text))
-        return result_list
+        return self.__trim_stop_word__(result_list)
 
     # 清空stop-word表
     def clear_stop_words(self):
@@ -35,6 +35,22 @@ class KeywordGenerator:
     def __read_word__(self):
         for line in open(self.file_path):
             self.stop_words.append(line.strip('\n'))
+
+    # 从key_list中删除stop-word
+    def __trim_stop_word__(self, key_list):
+        # 记录下标点的下标
+        index = 0
+        index_list = []
+        for key in key_list:
+            print 'test trim? = ' + key
+            if key in self.stop_words:
+                print 'trim added'
+                index_list.append(index)
+            index += 1
+        # 删除标点
+        for ix in index_list:
+            key_list.remove(ix)
+        return key_list
 
     '''
     返回stop word列表
