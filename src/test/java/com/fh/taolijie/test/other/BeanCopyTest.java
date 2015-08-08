aa7e0cc3c1d23ecb2ff4f8468985d135b5a2f927
whf <brucewhf@gmail.com>
2015-08-08 20:51:27 +0800

    update doc

---
+++ src/test/java/com/fh/taolijie/test/other/BeanCopyTest.java
@@ -0,0 +1,47 @@
+package com.fh.taolijie.test.other;
+
+import net.sf.cglib.beans.BeanCopier;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by whf on 8/8/15.
+ */
+public class BeanCopyTest {
+    @Test
+    public void testCopy() {
+        MyName myName = new MyName();
+        myName.setName("wanghognfei");
+
+        YourName yourName = new YourName();
+        BeanCopier bc = BeanCopier.create(MyName.class, YourName.class, false);
+
+        bc.copy(myName, yourName, null);
+        Assert.assertEquals(myName.getName(), yourName.getName());
+    }
+}
+
+class MyName {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class YourName {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
