3ce949c04391c7fca32c374cc99ede5237cacaa4
whf <brucewhf@gmail.com>
2015-08-14 11:09:10 +0800

    删除简历投递记录表的外键

---
+++ document/database/del-fk-2015-8-14.sql
@@ -0,0 +1,3 @@
+alter table taolijie.resume_post_rel drop foreign key fk_resume_post_rel_job_post1;
+alter table taolijie.resume_post_rel drop foreign key fk_resume_post_rel_member1;
+alter table taolijie.resume_post_rel drop foreign key fk_resume_post_rel_resume1;
