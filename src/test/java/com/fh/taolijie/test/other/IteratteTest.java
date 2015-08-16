04dcf6fda1243716c9db08e796fb722b4ece2fe2
whf <brucewhf@gmail.com>
2015-08-16 13:52:31 +0800

    添加编码规范文档

---
+++ src/test/java/com/fh/taolijie/test/other/IteratteTest.java
@@ -0,0 +1,31 @@
+package com.fh.taolijie.test.other;
+
+import com.fh.taolijie.domain.SysNotificationModel;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+/**
+ * Created by whf on 8/16/15.
+ */
+public class IteratteTest {
+    @Test
+    public void test() {
+        MyList m = new MyList();
+        for (String s : m.split()) {
+
+        }
+    }
+
+    class MyList {
+        public String[] split() {
+            System.out.println("split called");
+
+            return new String[] {
+                    "", "", ""
+            };
+        }
+
+    }
+
+}
