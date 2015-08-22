916cc6980165c7e0c008a574169491dc5fb366d8
whf <brucewhf@gmail.com>
2015-08-22 16:10:59 +0800

    添加登陆接口

--- src/main/java/com/fh/taolijie/controller/restful/admin/RestUserAdminController.java
+++ src/main/java/com/fh/taolijie/controller/restful/admin/RestUserAdminController.java
@@ -1,17 +1,17 @@
 package com.fh.taolijie.controller.restful.admin;
 
+import cn.fh.security.credential.Credential;
+import cn.fh.security.utils.CredentialUtils;
 import com.fh.taolijie.component.ListResult;
 import com.fh.taolijie.component.ResponseText;
+import com.fh.taolijie.constant.ErrorCode;
 import com.fh.taolijie.domain.MemberModel;
 import com.fh.taolijie.service.AccountService;
 import com.fh.taolijie.service.UserService;
 import com.fh.taolijie.utils.Constants;
 import com.fh.taolijie.utils.PageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by whf on 8/22/15.
@@ -24,6 +24,32 @@ public class RestUserAdminController {
 
     @Autowired
     UserService userService;
+
+
+    /**
+     * 给后台提供的登陆接口
+     * @return
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = Constants.Produce.JSON)
+    public ResponseText login(@RequestParam String username,
+                              @RequestParam String password) {
+        if (null == password) {
+            return new ResponseText(ErrorCode.BAD_PASSWORD);
+        }
+
+        MemberModel mem = accService.findMember(username, true);
+        if (null == mem) {
+            return new ResponseText(ErrorCode.USER_NOT_EXIST);
+        }
+
+        String hashPwd = CredentialUtils.sha(password);
+        if (false == hashPwd.equals(mem.getPassword())) {
+            return new ResponseText(ErrorCode.BAD_PASSWORD);
+
+        }
+
+        return new ResponseText(mem);
+    }
 
     /**
      * 查询所有用户
