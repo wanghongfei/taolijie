44574a09c6992d2a3303786b522bc38930f4ee6c
whf <brucewhf@gmail.com>
2015-09-01 20:29:04 +0800

    i添加一个sql文件

---
+++ document/database/create-table-recommend.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS `taolijie`.`recommended_post` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `job_id` INT NULL COMMENT '',
+  `sh_id` INT NULL COMMENT '',
+  `resume_id` INT NULL COMMENT '',
+  `apply_time` DATETIME NULL COMMENT '申请时间',
+  `validation` TINYINT(1) NULL DEFAULT 0 COMMENT '是否显示在首页',
+  `member_id` INT NULL COMMENT '申请人',
+  `memo` VARCHAR(300) NULL COMMENT '',
+  `order_index` INT NULL COMMENT '排序指数，值越小越靠前，从0开始',
+  `pass_time` DATETIME NULL COMMENT '通过审核的时间',
+  `remove_time` DATETIME NULL COMMENT '下架时间',
+  PRIMARY KEY (`id`)  COMMENT '',
+  INDEX `idx_reco_job` (`job_id` ASC)  COMMENT '',
+  INDEX `idx_reco_sh` (`sh_id` ASC)  COMMENT '',
+  INDEX `idx_reco_resume` (`resume_id` ASC)  COMMENT '',
+  INDEX `idx_reco_validation` (`validation` ASC)  COMMENT '',
+  INDEX `idx_reco_member` (`member_id` ASC)  COMMENT '',
+  INDEX `idx_reco_order` (`order_index` ASC)  COMMENT '')
+  ENGINE = InnoDB
