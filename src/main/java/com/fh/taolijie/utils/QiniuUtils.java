2080433a12c670243434fe0e28f9420b9727a558
whf <brucewhf@gmail.com>
2015-09-14 14:32:13 +0800

    添加7牛工具类和依赖

---
+++ src/main/java/com/fh/taolijie/utils/QiniuUtils.java
@@ -0,0 +1,58 @@
+package com.fh.taolijie.utils;
+
+import com.qiniu.api.auth.AuthException;
+import com.qiniu.api.auth.digest.Mac;
+import com.qiniu.api.io.IoApi;
+import com.qiniu.api.io.PutExtra;
+import com.qiniu.api.io.PutRet;
+import com.qiniu.api.rs.GetPolicy;
+import com.qiniu.api.rs.PutPolicy;
+import com.qiniu.api.rs.URLUtils;
+import org.apache.commons.codec.EncoderException;
+
+import java.io.File;
+
+/**
+ * 7牛云工具类
+ * Created by whf on 9/14/15.
+ */
+public class QiniuUtils {
+    public static final String ACCESS_KEY = "jiCcIJ426S8uHSN4fqL5OGjEmqSdENWTnk4Ze8I6";
+    public static final String SECRET_KEY = "rshYBtwtUWYCG5mcsFsHBnhz1vrgpaLKDymeNqiB";
+
+    public static final String SPACE_NAME = "taolijie";
+
+    public static Mac mac;
+
+    static {
+        mac = new Mac(ACCESS_KEY, SECRET_KEY);
+    }
+
+    public static void upload(String key) throws Exception {
+        PutPolicy putPolicy = new PutPolicy(SPACE_NAME);
+        String uptoken = putPolicy.token(mac);
+
+        PutExtra extra = new PutExtra();
+        String localFile = "/Users/whf/Pictures/4201347.jpeg";
+        PutRet ret = IoApi.putFile(uptoken, key, new File(localFile), extra);
+
+
+        System.out.println(ret);
+    }
+
+    /**
+     * 为资源生成URL
+     * @param domain
+     * @param key
+     * @return
+     * @throws EncoderException
+     * @throws AuthException
+     */
+    public static String genUrl(String domain, String key) throws EncoderException, AuthException {
+        String baseUrl = URLUtils.makeBaseUrl(domain, key);
+        GetPolicy getPolicy = new GetPolicy();
+        String downloadUrl = getPolicy.makeRequest(baseUrl, mac);
+
+        return downloadUrl;
+    }
+}
