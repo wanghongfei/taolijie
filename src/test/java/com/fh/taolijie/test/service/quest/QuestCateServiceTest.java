38345ce3b5a3de5e26ea1366dc8fba3769f74783
whf <brucewhf@gmail.com>
2015-09-20 11:08:16 +0800

    完成任务分类CRUD单元测试

--- src/test/java/com/fh/taolijie/test/service/quest/QuestCateServiceTest.java
+++ src/test/java/com/fh/taolijie/test/service/quest/QuestCateServiceTest.java
@@ -29,4 +29,61 @@ public class QuestCateServiceTest extends BaseSpringDataTestClass {
         Assert.assertNotNull(model);
         Assert.assertEquals("cate1", model.getName());
     }
+
+    @Test
+    public void testFindByName() {
+        QuestCategoryModel model = service.find("cate1");
+        Assert.assertNotNull(model);
+
+        model = service.find("hello");
+        Assert.assertNull(model);
+    }
+
+    @Test
+    public void testFindAll() {
+        ListResult<QuestCategoryModel> lr = service.findAll(0, 100);
+        Assert.assertNotNull(lr);
+        Assert.assertEquals(2, lr.getResultCount());
+
+        Assert.assertTrue(lr.getList().stream().anyMatch(cate -> cate.getName().equals("cate1")));;
+        Assert.assertTrue(lr.getList().stream().anyMatch(cate -> cate.getName().equals("cate2")));;
+
+
+        lr = service.findAll(0, 1);
+        Assert.assertNotNull(lr);
+        Assert.assertEquals(2, lr.getResultCount());
+
+        Assert.assertTrue(lr.getList().stream().anyMatch(cate -> cate.getName().equals("cate1")));;
+        Assert.assertFalse(lr.getList().stream().anyMatch(cate -> cate.getName().equals("cate2")));;
+    }
+
+    @Test
+    public void testAdd() {
+        QuestCategoryModel model = new QuestCategoryModel();
+        model.setName("cate3");
+        service.add(model);
+
+        model = service.find("cate3");
+        Assert.assertNotNull(model);
+        Assert.assertEquals("cate3", model.getName());
+    }
+
+    @Test
+    public void testDelete() {
+        service.delete(1);
+
+        Assert.assertNull(service.find(1));
+    }
+
+    @Test
+    public void testUpdate() {
+        QuestCategoryModel example = new QuestCategoryModel();
+        example.setId(1);
+        example.setName("new cate");
+        service.update(example);
+
+        example = service.find(1);
+        Assert.assertEquals("new cate", example.getName());
+    }
+
 }
