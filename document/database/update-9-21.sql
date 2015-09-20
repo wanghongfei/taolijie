0bdf42b3c25eb771ce194b4c2c80ce6a495d758f
whf <brucewhf@gmail.com>
2015-09-21 00:13:02 +0800

    调整quest表结构

---
+++ document/database/update-9-21.sql
@@ -0,0 +1,5 @@
+alter table taolijie.quest add column province_id int;
+alter table taolijie.quest add column city_id int;
+alter table taolijie.quest add column region_id int;
+alter table taolijie.quest add column college_id int;
+alter table taolijie.quest add column school_id int;
