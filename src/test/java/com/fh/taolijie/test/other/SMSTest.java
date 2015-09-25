dda031ff5c8cd275304decbcc03795fc5fdb6807
whf <brucewhf@gmail.com>
2015-09-25 20:06:05 +0800

    添加又拍签名算法

---
+++ src/test/java/com/fh/taolijie/test/other/SMSTest.java
@@ -0,0 +1,51 @@
+package com.fh.taolijie.test.other;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by whf on 9/25/15.
+ */
+public class SMSTest {
+    @Test
+    public void sendSMS() throws IOException {
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://api.weimi.cc/2/sms/send.html");
+
+        List<NameValuePair> parmList = new ArrayList<>();
+        parmList.add(new BasicNameValuePair("uid", "Ctpc2fjQKMq3"));
+        parmList.add(new BasicNameValuePair("pas", "7yaz2zeb"));
+        parmList.add(new BasicNameValuePair("mob", "18518369058"));
+        parmList.add(new BasicNameValuePair("cid", "d9Z7uIKCYqhQ"));
+        parmList.add(new BasicNameValuePair("p1", "wanghongfei"));
+        parmList.add(new BasicNameValuePair("type", "json"));
+
+        post.setEntity(new UrlEncodedFormEntity(parmList, HTTP.UTF_8));
+
+        HttpResponse resp = client.execute(post);
+        InputStream in = resp.getEntity().getContent();
+
+        InputStreamReader inReader = new InputStreamReader(in);
+        BufferedReader reader = new BufferedReader(inReader);
+
+        String line = null;
+        while ( (line = reader.readLine()) != null ) {
+            System.out.println(line);
+        }
+
+
+    }
+}
---
+++ src/test/java/com/fh/taolijie/test/other/UPaiTest.java
@@ -0,0 +1,25 @@
+package com.fh.taolijie.test.other;
+
+import com.fh.taolijie.utils.UpYunUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by whf on 9/25/15.
+ */
+public class UPaiTest {
+    @Test
+    public void testSign() {
+        Map<String, Object> parm = new HashMap<>();
+        parm.put("bucket", "demobucket");
+        parm.put("expiration", Integer.valueOf(1409200758) );
+        parm.put("save-key", "/img.jpg");
+
+
+        String sign = UpYunUtils.sign(parm, "cAnyet74l9hdUag34h2dZu8z7gU=");
+        Assert.assertEquals("646a6a629c344ce0e6a10cadd49756d4", sign);
+    }
+}
