acf81ca841ed86f34072c838d3ade95315d06461
whf <brucewhf@gmail.com>
2015-10-25 17:45:30 +0800

    补交

--- src/main/java/com/fh/taolijie/dto/LoginRespDto.java
+++ src/main/java/com/fh/taolijie/dto/LoginRespDto.java
@@ -1,12 +1,20 @@
 package com.fh.taolijie.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 /**
  * Created by whf on 10/3/15.
  */
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class LoginRespDto {
     private Integer id;
 
     private String appToken;
+
+    private String sid;
+
+    public LoginRespDto() {}
 
     public LoginRespDto(Integer id, String appToken) {
         this.id = id;
@@ -23,6 +31,14 @@ public class LoginRespDto {
 
     public String getAppToken() {
         return appToken;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
     }
 
     public void setAppToken(String appToken) {
