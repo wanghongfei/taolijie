b1fdf3c8d5896d0193960dab2d83f0562be756a2
whf <brucewhf@gmail.com>
2015-11-05 18:48:31 +0800

    更新日志分隔脚本

--- nginx-log.sh
+++ nginx-log.sh
@@ -5,10 +5,10 @@
 LOGS_PATH=/usr/local/nginx/logs
 
 ## 获取昨天的 yyyy-MM-dd
-YESTERDAY=$(date -d "yesterday" +%Y-%m-%d)
+YESTERDAY=$(date -d "yesterday" +%Y-%m-%d.%H-%M-%S)
 
 ## 移动文件
 mv ${LOGS_PATH}/access.log ${LOGS_PATH}/access_${YESTERDAY}.log
 
 ## 向 Nginx 主进程发送 USR1 信号。USR1 信号是重新打开日志文件
-kill -USR1 $(cat /usr/local/nginx/nginx.pid)
+kill -USR1 $(cat /usr/local/nginx/logs/nginx.pid)
