3339557e52d481fd393c431c88593c3bf5872cd4
whf <brucewhf@gmail.com>
2015-11-06 20:56:13 +0800

    添加pv查询接口

--- src/main/java/com/fh/taolijie/controller/restful/admin/RestStatisticsAdminController.java
+++ src/main/java/com/fh/taolijie/controller/restful/admin/RestStatisticsAdminController.java
@@ -1,13 +1,20 @@
 package com.fh.taolijie.controller.restful.admin;
 
 import com.fh.taolijie.component.ResponseText;
+import com.fh.taolijie.service.PVService;
 import com.fh.taolijie.service.StatisticsService;
 import com.fh.taolijie.servlet.OnlineListener;
 import com.fh.taolijie.utils.Constants;
+import com.fh.taolijie.utils.TimeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -19,8 +26,11 @@ public class RestStatisticsAdminController {
     @Autowired
     StatisticsService staService;
 
+    @Autowired
+    private PVService pvService;
 
     /**
+     * @deprecated
      * 页面访问量
      * @return
      */
@@ -32,6 +42,7 @@ public class RestStatisticsAdminController {
     }
 
     /**
+     * @deprecated
      * 在线人数
      * @return
      */
@@ -39,6 +50,20 @@ public class RestStatisticsAdminController {
     public ResponseText onlineStatistics() {
         Integer tot = OnlineListener.getOnlineAmount();
         return new ResponseText(tot);
+    }
+
+    /**
+     * 查询pv统计数据
+     * @return
+     */
+    @RequestMapping(value = "/pv", method = RequestMethod.GET, produces = Constants.Produce.JSON)
+    public ResponseText pv(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date start,
+                           @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date end) {
+
+        Date startTime = TimeUtil.calculateDate(start, Calendar.DAY_OF_MONTH, 1);
+        Date endTime = TimeUtil.calculateDate(end, Calendar.DAY_OF_MONTH, 1);
+
+        return new ResponseText(pvService.queryPv(startTime, endTime));
     }
 
 }
