11822bbd8f51776f336c7f48bd784677eba749a5
whf <brucewhf@gmail.com>
2015-11-30 10:21:47 +0800

    去掉任务表的几个无用字段

---
+++ document/database/update-2015-11-30.sql
@@ -0,0 +1,5 @@
+alter TABLE taolijie.quest drop column province_id;
+alter TABLE taolijie.quest drop column city_id;
+alter TABLE taolijie.quest drop column region_id;
+alter TABLE taolijie.quest drop column college_id;
+alter TABLE taolijie.quest drop column school_id;
