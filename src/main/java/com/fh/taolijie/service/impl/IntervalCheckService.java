41240de68ba1be688cd182cc6b2268f47473ce14
whf <brucewhf@gmail.com>
2015-11-30 17:37:57 +0800

    添加线下任务发布时间检查

--- src/main/java/com/fh/taolijie/service/impl/IntervalCheckService.java
+++ src/main/java/com/fh/taolijie/service/impl/IntervalCheckService.java
@@ -1,9 +1,15 @@
 package com.fh.taolijie.service.impl;
 
 import com.fh.taolijie.service.AccountService;
+import com.fh.taolijie.utils.Constants;
+import com.fh.taolijie.utils.JedisUtils;
+import com.fh.taolijie.utils.StringUtils;
 import com.fh.taolijie.utils.TimeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Pipeline;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -16,6 +22,59 @@ public class IntervalCheckService {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private JedisPool jedisPool;
+
+    /**
+     * 线下任务发布时间间隔检查key值
+     */
+    private final String OFF_QUEST_INTERVAL_PREFIX = "interval" + Constants.DELIMITER + "offquest";
+
+    /**
+     * 标记线下任务发布时间点, 用于发布间隔检查
+     * @param memId
+     */
+    public final void markOffQuestPost(Integer memId) {
+        // 生成redis key
+        String key = genKey(memId);
+
+
+        Jedis jedis = JedisUtils.getClient(jedisPool);
+
+        Pipeline line = jedis.pipelined();
+        line.set(key, "T");
+        line.expire(key, 15); // 15s
+
+        line.sync();
+        JedisUtils.returnJedis(jedisPool, jedis);
+    }
+
+    /**
+     * 检查线下任务时间间隔是否合法
+     * @param memId
+     * @return
+     */
+    public final boolean checkOffQuestPostInterval(Integer memId) {
+        String key = genKey(memId);
+
+        Jedis jedis = JedisUtils.getClient(jedisPool);
+        String val = jedis.get(key);
+        JedisUtils.returnJedis(jedisPool, jedis);
+
+        return !StringUtils.checkNotEmpty(val);
+    }
+
+    private String genKey(Integer memId) {
+        return StringUtils.concat(OFF_QUEST_INTERVAL_PREFIX.length() + 10, OFF_QUEST_INTERVAL_PREFIX, Constants.DELIMITER, memId);
+    }
+
+    /**
+     * 检查时间间隔是否足够
+     * @param lastDate 较早的时间点
+     * @param interval 时间间隔值
+     * @param unit 时间间隔的单位, {@link java.util.Calendar}中定义的常量
+     * @return
+     */
     public final boolean checkInterval(Date lastDate, int interval, TimeUnit unit) {
         Date nowTime = new Date();
         if (null == lastDate) {
