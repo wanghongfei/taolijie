5694783e80b5a609ce51820f16d78300fead1e18
whf <brucewhf@gmail.com>
2016-01-06 00:16:42 +0800

    删除导致编译错误的import

--- src/main/java/com/fh/taolijie/service/impl/DefaultAccountService.java
+++ src/main/java/com/fh/taolijie/service/impl/DefaultAccountService.java
@@ -34,7 +34,6 @@ import org.springframework.transaction.annotation.Transactional;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Pipeline;
-import sun.jvmstat.perfdata.monitor.CountedTimerTaskUtils;
 
 import java.util.Date;
 import java.util.HashMap;
